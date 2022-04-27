0a5bd0a1207717f05265adbe68913ad2c961110f
mkuenzl <33060049+mkuenzl@users.noreply.github.com>
2022-04-27 18:55:50 +0200

    added process information

--- script/retrieve_stats.py
+++ script/retrieve_stats.py
@@ -1,17 +1,26 @@
 import subprocess
+#https://phoenixnap.com/kb/linux-commands-check-memory-usage
+#https://www.baeldung.com/linux/monitor-disk-io
+#https://www.elektronik-kompendium.de/sites/raspberry-pi/1911241.htm#:~:text=Prozessor%2DAuslastung&text=Die%20aktuelle%20Gesamtauslastung%20kann%20man,die%20Liste%20nach%20der%20Speicherbelegung.
+
+def process_information():
+    proc = subprocess.check_output("ps axo pid,cpu,%cpu,%mem", shell=True)
+    process_data = proc.decode('UTF-8')
+    print(process_data)
 
 
-def pingAddress():
+def ping_information():
     proc = subprocess.check_output("ping -c 4 www.stackoverflow.com | tail -1 | awk '{print $4}'", shell=True)
-    ping_information = proc.decode('UTF-8').split('/')
-    min = ping_information[0]
-    avg = ping_information[1]
-    max = ping_information[2]
-    deviation = ping_information[3]
+    ping_data = proc.decode('UTF-8').split('/')
+    min = ping_data[0]
+    avg = ping_data[1]
+    max = ping_data[2]
+    deviation = ping_data[3]
 
     print(f"pint output: {min}, {avg}, {max}, {deviation}")
 
 
 if __name__ == '__main__':
-    pingAddress()
+    ping_information()
+    process_information()
 
